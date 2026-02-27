957fe4100192eaf1c4d8dc5bd37f188fbebefa7a
Thorn Walli <lammpee@gmail.com>
2026-02-27 13:46:25 +0100

    fix(example): update import statements and tsconfig paths for exampleFactory mixin

--- packages/core/src/components/example-with-pkg-mixin/example-with-pkg-mixin.tsx
+++ packages/core/src/components/example-with-pkg-mixin/example-with-pkg-mixin.tsx
@@ -1,6 +1,15 @@
+// eslint-disable-next-line import-x/order
 import { Component, h, Host, Mixin, Prop } from '@stencil/core';
-// import { exampleFactory } from '../../../../utilities/src/mixins/example'; // ✅
-import { exampleFactory } from '@boilerplate-stencil/utilities/mixins/example'; // ❌
+
+// ✅ Works but is no extern external import
+// import { exampleFactory } from '../../../../utilities/src/mixins/example';
+
+// ⚠️ Works with warning Bundling Warning undefined Could not resolve import "#utilities/mixins/example" in
+// Alias from relative path
+// import { exampleFactory } from '#utilities/mixins/example';
+
+// ❌ Does not work, no props from mixin available
+import { exampleFactory } from '@boilerplate-stencil/utilities/mixins/example';
 
 @Component({
   tag: 'example-with-pkg-mixin',
